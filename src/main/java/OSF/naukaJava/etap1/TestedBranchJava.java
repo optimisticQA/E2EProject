a81d1b0249182913a7819570c8ad0d12e8a2a788
Artur Łubkowski <artur.lubkowski@opi.org.pl>
2021-04-16 15:40:27 +0200

    Update TestedBranchJava.java

--- src/main/java/OSF/naukaJava/etap1/TestedBranchJava.java
+++ src/main/java/OSF/naukaJava/etap1/TestedBranchJava.java
@@ -4,7 +4,7 @@ public class TestedBranchJava {
     //Zmiana 1
     //Zmiana 3
     //Zmiana 4
-
+    //Zmiana 1 //Zmiana 1 //Zmiana 1 //Zmiana 1 //Zmiana 1 //Zmiana 1 //Zmiana 1 //Zmiana 1
 
 
     //Zmiana 2
